a4542b62c99412d6186481dbd69f249d09e515e3
JibranH24 <jibranhussain2408@gmail.com>
2022-12-21 22:56:05 +0000

    created intro screen

---
+++ app/src/main/java/com/example/j_commerce/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.j_commerce.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.j_commerce.R
+
+class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/com/example/j_commerce/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.j_commerce.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.j_commerce.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+}
