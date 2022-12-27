08731e540e71c089c91ba59aedf07beb8c4a4005
JibranH24 <jibranhussain2408@gmail.com>
2022-12-27 18:19:25 +0000

    added navbar

---
+++ app/src/main/java/com/example/j_commerce/fragments/shopping/CartFragment.kt
@@ -0,0 +1,7 @@
+package com.example.j_commerce.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.j_commerce.R
+
+class CartFragment : Fragment(R.layout.fragment_cart){
+}
---
+++ app/src/main/java/com/example/j_commerce/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.j_commerce.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.j_commerce.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile){
+}
