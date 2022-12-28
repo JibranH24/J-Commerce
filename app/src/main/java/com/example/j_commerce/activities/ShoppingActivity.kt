0a462d73a8fd8bc2a376c852500ef637155c9082
JibranH24 <jibranhussain2408@gmail.com>
2022-12-28 19:47:45 +0000

    created special products line

--- app/src/main/java/com/example/j_commerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/j_commerce/activities/ShoppingActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.j_commerce.R
 import com.example.j_commerce.databinding.ActivityShopingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
     val binding by lazy{
         ActivityShopingBinding.inflate(layoutInflater)
